7e8e7b4cf8f10842ebf1aa620e4f5060968905d9
SU <SU@R409C6>
2022-04-05 09:02:29 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PZ_7_htlfrnjh
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Button_Click_save(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_new(object sender, RoutedEventArgs e)
+        {
+            CreateFileWindow createFileWindow = new CreateFileWindow();
+            if (createFileWindow.ShowDialog() == true)
+            {
+                File.Create(@"G:\PZ_7_htlfrnjh\PZ_7_htlfrnjh\Data\" + $"{createFileWindow.FileName}" + ".txt");
+            }
+        }
+
+
+    }
+
+}
